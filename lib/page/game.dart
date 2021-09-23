2945cceb4382e72aa7ee6c54eee4fbd55aaf5556
Adam <adamj500@interia.pl>
2021-09-23 21:10:18 +0200

    flutter upgrade

--- lib/page/game.dart
+++ lib/page/game.dart
@@ -1,5 +1,6 @@
-import 'package:alkochin/widgets/timer_button.dart';
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:stop_watch_timer/stop_watch_timer.dart';
 
 class Game extends StatefulWidget {
   const Game({Key key}) : super(key: key);
@@ -9,15 +10,33 @@ class Game extends StatefulWidget {
 }
 
 class _GameState extends State<Game> {
+  List<int> blackTeamShots = new List(2);
+  List<int> whiteTeamShots = new List(2);
+  List<bool> playerPlaying = [true, false, false];
+  List<double> screenSize = List(2);
+  bool isWhite;
+  int timeAmount;
+
+  @override
+  void initState() {
+    super.initState();
+    blackTeamShots = [0, 0];
+    whiteTeamShots = [0, 0];
+    isWhite = true;
+    timeAmount = ModalRoute.of(context).settings.arguments;
+  }
+
   @override
   Widget build(BuildContext context) {
-    final int timer = ModalRoute.of(context).settings.arguments;
+    Duration countdownDuration = Duration(minutes: timeAmount);
+    screenSize[0] = MediaQuery.of(context).size.width;
+    screenSize[1] = MediaQuery.of(context).size.height * 0.415;
+
     return Scaffold(
       backgroundColor: Colors.grey[300],
       body: SafeArea(
         child: Column(
           children: [
-            TimerButton(time: timer),
             Divider(
               height: 10.0,
               thickness: 3.0,
@@ -27,16 +46,18 @@ class _GameState extends State<Game> {
               height: 10.0,
               thickness: 3.0,
             ),
-            TimerButton(time: timer)
           ],
         ),
       ),
     );
   }
 
+  Widget timerButton(int timeAmount, bool isWhite) {
+    final stopWatch = StopWatchTimer();
+  }
+
   Widget pieceButtons() {
     final barHeight = 70.0;
-    final imageSize = 50.0;
 
     return Container(
       height: barHeight,
@@ -44,44 +65,106 @@ class _GameState extends State<Game> {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          IconButton(
-              icon: Image.asset(
-                'pawn.png',
-              ),
-              iconSize: imageSize,
-              onPressed: null),
-          IconButton(
-              icon: Image.asset(
-                'knight.png',
-              ),
-              iconSize: imageSize,
-              onPressed: null),
-          IconButton(
-              icon: Image.asset(
-                'bishop.png',
-              ),
-              iconSize: imageSize,
-              onPressed: null),
-          IconButton(
-              icon: Image.asset(
-                'rook.png',
-              ),
-              iconSize: imageSize,
-              onPressed: null),
-          IconButton(
-              icon: Image.asset(
-                'queen.png',
-              ),
-              iconSize: imageSize,
-              onPressed: null),
-          IconButton(
-              icon: Image.asset(
-                'king.png',
-              ),
-              iconSize: imageSize - 15,
-              onPressed: null),
+          pieceIcon(
+              'pawn.png',
+              1,
+              playerPlaying[0] == true
+                  ? null
+                  : playerPlaying[1] == true
+                      ? true
+                      : playerPlaying[2] == true
+                          ? false
+                          : null),
+          pieceIcon(
+              'knight.png',
+              3,
+              playerPlaying[0] == true
+                  ? null
+                  : playerPlaying[1] == true
+                      ? true
+                      : playerPlaying[2] == true
+                          ? false
+                          : null),
+          pieceIcon(
+              'bishop.png',
+              3,
+              playerPlaying[0] == true
+                  ? null
+                  : playerPlaying[1] == true
+                      ? true
+                      : playerPlaying[2] == true
+                          ? false
+                          : null),
+          pieceIcon(
+              'rook.png',
+              5,
+              playerPlaying[0] == true
+                  ? null
+                  : playerPlaying[1] == true
+                      ? true
+                      : playerPlaying[2] == true
+                          ? false
+                          : null),
+          pieceIcon(
+              'queen.png',
+              9,
+              playerPlaying[0] == true
+                  ? null
+                  : playerPlaying[1] == true
+                      ? true
+                      : playerPlaying[2] == true
+                          ? false
+                          : null),
+          pieceIcon(
+              'king.png',
+              7,
+              playerPlaying[0] == true
+                  ? null
+                  : playerPlaying[1] == true
+                      ? true
+                      : playerPlaying[2] == true
+                          ? false
+                          : null,
+              30.0),
         ],
       ),
     );
+  }
+
+  Widget pieceIcon(String path, int shots, bool isWhite,
+      [double imageSize = 50.0]) {
+    if (isWhite != null) {
+      return IconButton(
+          icon: Image.asset(path),
+          iconSize: imageSize,
+          onPressed: () {
+            switch (shots) {
+              case 1:
+              case 3:
+              case 5:
+              case 7:
+              case 9:
+                if (isWhite) {
+                  whiteTeamShots[0] = whiteTeamShots[1];
+                  whiteTeamShots[1] += shots;
+                } else {
+                  blackTeamShots[0] = blackTeamShots[1];
+                  blackTeamShots[1] += shots;
+                }
+                break;
+              default:
+                break;
+            }
+          });
+    } else {
+      return Opacity(
+        opacity: 0.5,
+        child: IconButton(
+          icon: Image.asset(path),
+          iconSize: imageSize,
+          onPressed: null,
+        ),
+      );
+    }
   }
 }
--- lib/widgets/timer_button.dart
+++ lib/widgets/timer_button.dart
@@ -18,6 +18,9 @@ class _TimerButtonState extends State<TimerButton> {
   String timerAsString;
   int decimalPart;
 
+  Duration duration;
+  bool swapPlayer = false;
+
   void startGame() {
     if (time != null) {
       time.cancel();
@@ -34,16 +37,29 @@ class _TimerButtonState extends State<TimerButton> {
   @override
   void initState() {
     super.initState();
+    // resetTimer();
     timer = widget.time;
     timerAsString = timer.toStringAsFixed(2);
     int indexOfDecimal = timerAsString.indexOf('.');
     decimalPart = int.tryParse(timerAsString.substring(indexOfDecimal)) ?? 0;
   }
 
+  // void resetTimer() {
+  //   setState(() {
+  //     this.duration = new Duration(minutes: widget.time);
+  //   });
+  // }
+
+  // void startTimer() {
+  //   this.timer = Timer.periodic(Duration(seconds: 1), (_) => addTime());
+  // }
+
+  void addTime() {}
+
   @override
   Widget build(BuildContext context) {
     screenSize[0] = MediaQuery.of(context).size.width;
-    screenSize[1] = MediaQuery.of(context).size.height * 0.43;
+    screenSize[1] = MediaQuery.of(context).size.height * 0.415;
 
     //TODO: Zrotatowac dla odpowiedniej strony
 
